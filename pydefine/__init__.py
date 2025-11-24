f94bab3efa0331001f8bced8bf26be07de25bc33
mdyahhya <142885726+mdyahhya@users.noreply.github.com>
2025-11-24 21:33:58 +0530

    Update __init__.py

--- pydefine/__init__.py
+++ pydefine/__init__.py
@@ -9,36 +9,35 @@ explanations with fix suggestions.
 Basic usage:
 
    >>> import pydefine
-   >>> result = pydefine.decode_traceback(some_traceback_string)
-   >>> print(result['simple_explanation'])
+   >>> # That's it! Uncaught exceptions will be explained automatically.
 
-   >>> try:
-   ...     1 / 0
-   ... except Exception as e:
-   ...     result = pydefine.decode_exception(e)
-   ...     print(result['simple_explanation'])
-
-Full documentation is available at https://github.com/mdyahhya/pydefine
+Full documentation:
+https://github.com/mdyahhya/pydefine
 
 :copyright: (c) 2025 by Yahya.
 :license: MIT, see LICENSE for more details.
 """
 
 import sys
-import traceback as tb_module
 
 from .version import __version__
-from .core import decode_traceback, decode_exception, safe_run, decode_traceback_file, explain, quick_decode
-from .mapping import get_exception_info, EXCEPTION_MAP, list_all_exceptions, search_exceptions_by_tag, TOTAL_EXCEPTIONS, ALL_TAGS
-from .utils import extract_error_info, format_output, tokenize_output, strip_ansi_codes, get_python_version, get_error_category, clean_traceback_text
+from .core import (
+    decode_traceback, decode_exception, safe_run, decode_traceback_file, 
+    explain, quick_decode
+)
+from .mapping import (
+    get_exception_info, EXCEPTION_MAP, list_all_exceptions, 
+    search_exceptions_by_tag, TOTAL_EXCEPTIONS, ALL_TAGS
+)
+from .utils import (
+    extract_error_info, format_output, tokenize_output, strip_ansi_codes,
+    get_python_version, get_error_category, clean_traceback_text
+)
 from .cli import main as cli_main
 from .i18n import translate_explanation, set_language, get_language, SUPPORTED_LANGUAGES
 
-# Public API
 __all__ = [
-    # Version
     "__version__",
-    
     # Core functions
     "decode_traceback",
     "decode_exception",
@@ -46,16 +45,14 @@ __all__ = [
     "decode_traceback_file",
     "explain",
     "quick_decode",
-    
-    # Mapping functions
+    # Mapping
     "get_exception_info",
     "EXCEPTION_MAP",
     "list_all_exceptions",
     "search_exceptions_by_tag",
     "TOTAL_EXCEPTIONS",
     "ALL_TAGS",
-    
-    # Utility functions
+    # Utilities
     "extract_error_info",
     "format_output",
     "tokenize_output",
@@ -63,10 +60,8 @@ __all__ = [
     "get_python_version",
     "get_error_category",
     "clean_traceback_text",
-    
     # CLI
     "cli_main",
-    
     # i18n
     "translate_explanation",
     "set_language",
@@ -74,11 +69,14 @@ __all__ = [
     "SUPPORTED_LANGUAGES",
 ]
 
-# Display branding message on import
-def _display_branding():
-    """Print branding message when library is imported."""
-    branding = "\n✨ Powered by pyDefine ● Created by Yahya ✨\n"
-    print(branding, file=sys.stderr)
+# Automatically install global exception hook on import
+def _global_exception_hook(exc_type, exc_value, exc_traceback):
+    try:
+        from .core import decode_exception
+        decoded = decode_exception(exc_value)
+        print(decoded['formatted_output'])
+    except Exception:
+        # If something fails, fallback to default Python behavior
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
 
-# Automatically display branding on import
-_display_branding()
+sys.excepthook = _global_exception_hook
