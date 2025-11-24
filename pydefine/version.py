36ebe11aee8e937b7b295c7cdcd784710879bd25
mdyahhya <142885726+mdyahhya@users.noreply.github.com>
2025-11-24 18:55:13 +0530

    Create version.py

---
+++ pydefine/version.py
@@ -0,0 +1,20 @@
+"""
+pydefine.version
+~~~~~~~~~~~~~~~~
+
+Version information for pyDefine library.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Yahya"
+__email__ = "yahyabuilds@gmail.com"
+__license__ = "MIT"
+__url__ = "https://github.com/mdyahhya/pydefine"
+__description__ = "Convert Python errors into beginner-friendly explanations"
+
+# Version info tuple
+VERSION = tuple(map(int, __version__.split('.')))
+
+# Library metadata
+LIBRARY_NAME = "pyDefine"
+PACKAGE_NAME = "pydefine"
